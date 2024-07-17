25e7e8b8e76e5e845268ccffac4a59b2867589ae
ndvp39 <ndvp39@gmail.com>
2024-07-17 23:02:01 +0300

    upload 7

--- server/server.js
+++ server/server.js
@@ -86,6 +86,7 @@ app.get('/verify', (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
