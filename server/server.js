8ff8a665234642536dd52fde9cf96ee7a8f94eee
ndvp39 <ndvp39@gmail.com>
2024-07-19 00:45:36 +0300

    upload 36

--- server/server.js
+++ server/server.js
@@ -179,6 +179,7 @@ app.get('/api/verify', (req, res) => {
                     <div class="container">
                       <h1>Your account was succesfully verified!</h1>
                       <p>Thank you for register to our website!</p>
+                      <a href="/api/register">Login Now</a>
                     </div>
                   </body>
                 </html>
